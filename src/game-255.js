3fe18d9e74f50723fec3cff485b6d7722fbc57c6
tangs <tangsong1988kaka@gmail.com>
2023-10-19 14:57:43 +0800

    add game 255 test.

---
+++ src/game-255.js
@@ -0,0 +1,30 @@
+const test = require('./test_339_temp');
+
+const mysql = require('mysql');
+const api = require('./api');
+
+const connection = mysql.createConnection({     
+    host     : '192.168.1.211',       
+    user     : 'root',              
+    password : 'a123123',       
+    port: '3306',                   
+    database: 'ns_games' 
+}); 
+
+const main = async () => {
+    const gameId = 255;
+    const testTimesPerCell = 100;
+    const configs = await api.loadConfigs(connection, gameId);
+    const startTime = Date.now();
+    
+    for (const row of configs) {
+        const {level_id, bet_money} = row;
+        console.log(`test level id: ${level_id}, bet money: ${bet_money}`);
+        await test.test(gameId, bet_money, level_id, 0, testTimesPerCell);
+    }
+
+    const useMilliSeconds = Date.now() - startTime;
+    console.log(`finish test game ${gameId}, types: ${configs.length}, use seconds: ${useMilliSeconds / 1000}`);
+}
+
+main();
